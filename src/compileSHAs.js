76aa7c01b1695add95a3e34f1386a920aed2d014
Zyclotrop-j <Zyclotrop-j@users.noreply.github.com>
2018-12-15 15:33:52 +0100

    Create compileSHAs.js

---
+++ src/compileSHAs.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+const walkSync = (dir, filelist = []) => {
+  fs.readdirSync(dir).forEach(file => {
+
+    filelist = fs.statSync(path.join(dir, file)).isDirectory()
+      ? walkSync(path.join(dir, file), filelist)
+      : filelist.concat(path.join(dir, file));
+
+  });
+return filelist;
+}
